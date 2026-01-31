55b43aaa94119706c9c8a76dbf773728c156e057
rehanraja-dev <rehan2672002@gmail.com>
2026-02-01 01:29:34 +0530

    2 lecture completed

---
+++ basics/01_variable.js
@@ -0,0 +1,20 @@
+const accountId = 144
+let accountEmail = "rehan@gmail.com"
+var password = "1234"
+accountCity = "Bhopal"
+let accountState;
+
+accountEmail = "h@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+// console.log(accountEmail);
+// console.log(accountPassword);
+
+/*
+    dont use var 
+    because of block scope and functional scope
+*/
+
+console.table([accountEmail,accountId,accountCity,accountPassword, accountState])
+
---
+++ basics/02_datatypes.js
@@ -0,0 +1,16 @@
+console.log(typeof "rehan")
+console.log(typeof null)
+
+// number = 2 to power 53
+
+// bigint
+
+// string => "something"
+
+// boolean => true/false
+
+// null => standalone value
+
+//  undefined
+
+// symbol => unique
